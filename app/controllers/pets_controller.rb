51115b52d777dfaad8bcbe7d01a0f9cfe3577cd0
Antonio Reid <read.musik@gmail.com>
2018-04-09 03:38:02 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,7 +28,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
-  
+
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
