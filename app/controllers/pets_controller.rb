dcf6b8d286da0f00a67948073a35c34c3aae40bb
Antonio Reid <read.musik@gmail.com>
2018-04-09 03:17:22 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do
     #binding.pry
     @pet = Pet.create(params[:pet])
-    binding.pry
+    #binding.pry
     if !params[:owner][:owner_name].empty?
       @pet.owner = Owner.create(name: params[:owner][:owner_name])
     end
